89c8bdfef927f201c2b5800f4784c6bcb1f61f66
Joaquim Neto <joaquimnetoengcivil@gmail.com>
2026-08-02 10:13:01 -0400

    Update Field Time multi-job workflow

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3908769292" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "727711377" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
